ee1bb43a10df7e7c106eb88c13321410a548b747
ilel-hla <iliaselhal1@gmail.com>
2025-03-12 18:11:07 +0000

    SO_LONG

--- bonus/out_screen_bonus.c
+++ bonus/out_screen_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ilel-hla <ilel-hla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 02:25:10 by ilel-hla          #+#    #+#             */
-/*   Updated: 2025/03/12 17:48:27 by ilel-hla         ###   ########.fr       */
+/*   Updated: 2025/03/12 18:09:59 by ilel-hla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	ft_out_of_screen(t_map *map)
 {
 	if (map->width > 80 || map->height > 41)
 	{
-		ft_free(map->map);
-		free(map);
 		ft_error_free(map, "Error\nmap too big lol\n");
 	}
 }
